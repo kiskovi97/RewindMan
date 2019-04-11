79435b5b2f5298cced54733bd4fe325a3c7f1a7b
kiskovi97 <kiskovi97@gmail.com>
2019-04-11 09:55:36 +0200

    Menu 2.0

---
+++ RewindMan/Assets/UI/CameraShaking.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaking : MonoBehaviour
+{
+    private Vector3 basePos;
+    public float size = 2.0f;
+    private float angle = 0.0f;
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        basePos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += Time.deltaTime * speed;
+        transform.position = getPostion(angle);
+    }
+
+    Vector3 getPostion(float angle)
+    {
+        return basePos + Mathf.Cos(angle) * Vector3.left + Mathf.Sin(angle * 3) / 3 * Vector3.down;
+    }
+}
