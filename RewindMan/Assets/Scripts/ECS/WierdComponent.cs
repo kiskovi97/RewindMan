e2c6ec5e570946cb095c6ca22286dcab4539f8ec
kiskovi97 <kiskovi97@gmail.com>
2019-04-20 11:51:05 +0200

    ECS Example

---
+++ RewindMan/Assets/Scripts/ECS/WierdComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class WierdComponent : MonoBehaviour
+{
+    public float speed = 15f;
+}
+
+class WierdParticleSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach<WierdComponent, Transform>((Entity entity, WierdComponent p, Transform e) => {
+            Debug.Log("In the foreach");
+            e.Rotate(0f, p.speed * Time.deltaTime, 0f);
+        });
+    }
+}
