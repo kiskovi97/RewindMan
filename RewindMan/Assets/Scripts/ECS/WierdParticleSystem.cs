923a402bbcd7c244969713fbf2bf9475d14e8054
kiskovi97 <kiskovi97@gmail.com>
2019-04-20 13:55:25 +0200

    Fixed Update Try
    No can do. Its working like an Update. Can't work as a FixedUpdate

---
+++ RewindMan/Assets/Scripts/ECS/WierdParticleSystem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using UnityEngine.Experimental.PlayerLoop;
+
+namespace Assets.Scripts.ECS
+{
+    [UpdateAfter(typeof(FixedUpdate))]
+    class WierdParticleSystem : ComponentSystem
+    {
+        float prevTime = 0;
+
+        protected override void OnUpdate()
+        {
+            float delta = Time.time - prevTime;
+            Debug.Log("UpdateAfter: " + delta);
+            Entities.ForEach<WierdComponent, Transform>((Entity entity, WierdComponent p, Transform e) => {
+                e.Translate(Time.fixedDeltaTime, 0f, 0f);
+            });
+            prevTime = Time.time;
+        }
+    }
+    
+    [UpdateBefore(typeof(FixedUpdate))]
+    class WierdParticleSystem2 : ComponentSystem
+    {
+        float prevTime = 0;
+
+        protected override void OnUpdate()
+        {
+            float delta = Time.time - prevTime;
+            Debug.Log("UpdateBefore: " + delta);
+            Entities.ForEach<WierdComponent, Transform>((Entity entity, WierdComponent p, Transform e) => {
+                e.Translate(Time.fixedDeltaTime, 0f, 0f);
+            });
+            prevTime = Time.time;
+        }
+    }
+}
