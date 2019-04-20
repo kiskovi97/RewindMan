0c3d9acc1aed28638c015bfc6f530e943a90019e
kiskovi97 <kiskovi97@gmail.com>
2019-04-20 18:36:12 +0200

    Falling and Levels Fix

--- RewindMan/Assets/Scripts/PhysicsSystem02/Objects/MovingObject.cs
+++ RewindMan/Assets/Scripts/PhysicsSystem02/Objects/MovingObject.cs
@@ -6,12 +6,15 @@ namespace FixPhysics
     public abstract class MovingObject : RecordedObject<MovingState>
     {
         public Vector3 startVelocity = new Vector3(0, 1, 0);
+        private static float maxFallingSpeedIn = 20f;
+        public Fix maxFallingSpeed;
 
         protected virtual void Start()
         {
             FixVec3 Position = FixConverter.ToFixVec3(transform.position);
             FixVec3 Velocity = FixConverter.ToFixVec3(startVelocity);
             state = new MovingState(Velocity, Position);
+            maxFallingSpeed = FixConverter.ToFix(maxFallingSpeedIn);
         }
 
         public override void Update()
@@ -25,6 +28,7 @@ namespace FixPhysics
         {
             state.position = position;
             state.velocity = velocity;
+            if (state.velocity.Y < (maxFallingSpeed * -1)) state.velocity = new FixVec3(state.velocity.X, maxFallingSpeed * -1, state.velocity.Z);
         }
 
         protected void PositionCorrection(FixVec3 newPosition)
@@ -36,11 +40,13 @@ namespace FixPhysics
         {
             state.velocity = newVelocity;
             if (newVelocity.GetMagnitude() < FixWorld.deltaTime * FixWorld.gravity.GetMagnitude()) newVelocity = FixVec3.Zero;
+            if (state.velocity.Y < (maxFallingSpeed * -1)) state.velocity = new FixVec3(state.velocity.X, maxFallingSpeed * -1, state.velocity.Z);
         }
 
         protected void Accelerate(FixVec3 sumForce)
         {
             state.velocity += sumForce * FixWorld.deltaTime;
+            if (state.velocity.Y < (maxFallingSpeed * -1)) state.velocity = new FixVec3(state.velocity.X, maxFallingSpeed * -1, state.velocity.Z);
         }
 
         public abstract void Move();
